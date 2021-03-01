ad70e88aef91be97733cd084e077aa16a8d7358b
Yesid Sanchez <yesids@tissini.com>
2021-03-01 16:01:43 -0500

    add isNetwork

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,29 @@
 import { IonApp } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import getWebview from './composables/webview'
+import { Plugins } from '@capacitor/core';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp
   },
-  setup(props) {
+  async setup() {
+      const { Network,Toast,App } = Plugins;
+      const isConected = await Network.getStatus();
+
       const {getWeb} = getWebview()
-      getWeb()
+     
+      if(isConected.connected) getWeb()
+      else {
+          Toast.show({
+            text : 'por favor conectese a una red con internet',
+            duration: 'long'
+          })
+
+          App.exitApp()
+      }
+
   }
 
 
